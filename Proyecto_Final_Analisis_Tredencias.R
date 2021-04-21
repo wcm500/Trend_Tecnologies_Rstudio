d840ea1369eeff6e6dba7e079a5763b998c22da3
unknown <wcourraum248@ulacit.ed.cr>
2021-04-20 20:22:27 -0600

    kkk

--- Proyecto_Final_Analisis_Tredencias.R
+++ Proyecto_Final_Analisis_Tredencias.R
@@ -1,56 +1,65 @@
-#install.packages("readtext")
 library(readtext)
 library(tm)
 library(stringr)
 library(ggplot2)
-#install.packages("ggrepel")
-#install.packages("syuzhet")
 library(syuzhet)
 library(ggrepel)
 library(dplyr)
 library("wordcloud")
 #Crear la corpora con los PDFs 
-
+#Integrantes
+#William Courrau Martinez
+#Michael Monge
+#Adrian Campos Artavia
 #________________________________________________________________________________
 #Analisis con corpus 
 rtext <- readtext("D:/Mineria de datos/PROYECTO FINAL/Trend_Tecnologies_Rstudio/PDF Technology")
-
+#Agregar un corpus
 rtext_corpus <- VCorpus(VectorSource(rtext$text))
-
+#minuscula
 rtext_corpus <- tm_map(rtext_corpus, content_transformer(tolower))
-
+#Eliminar numeros
 rtext_corpus <- tm_map(rtext_corpus, removeNumbers) 
-
+#Agregar mis propias  stopwords
 myStopWords<- c("will","use","can","technolog","new")
+#Eliminar stopwords
+rtext_corpus <- tm_map(rtext_corpus, removeWords, c(stopwords("english"),"technolog")) 
 
-rtext_corpus <- tm_map(rtext_corpus, removeWords, c(stopwords("english"),myStopWords)) 
+rtext_corpus <- tm_map(rtext_corpus, removeWords, c(stopwords("english"),"will")) 
 
+rtext_corpus <- tm_map(rtext_corpus, removeWords, c(stopwords("english"),"can")) 
+
+rtext_corpus <- tm_map(rtext_corpus, removeWords, c(stopwords("english"),"new")) 
+
+#Eliminar signos de puntuacion
 rtext_corpus <- tm_map(rtext_corpus, removePunctuation)
-
+#Eliminar espacios en blanco
 rtext_corpus <- tm_map(rtext_corpus, stemDocument)
 
 rtext_corpus <- tm_map(rtext_corpus, stripWhitespace) 
-
+#Tokenizar
 rtext_data_dtm <- DocumentTermMatrix(rtext_corpus)
 
 rtext_data_dtm <- removeSparseTerms(rtext_data_dtm,0.01) # 1% de uso menor eliminado
 rtext_data_dtm
 
-freq <- sort(colSums(as.matrix(rtext_data_dtm)), decreasing=TRUE)  
+freq <- sort(colSums(as.matrix(rtext_data_dtm)), decreasing=TRUE)  # Graficar la frecuencia de la palabras
 freq
 
-wfd <- data.frame(word=names(freq), freq=freq)
-head(wf)
+wfd <- data.frame(word=names(freq), freq=freq) # pasar a dataframe
+head(wfd)
 
+
+#Grafica de palabras frecuentes
 ggplot(subset(wfd, freq>30), aes(x = reorder(word, -freq), y = freq)) +
   geom_bar(stat="identity", fill="steelblue")+
   geom_text(aes(label=freq), vjust=1.6, color="white", size=3.5)+
-  theme(axis.text.x=element_text(angle=-90, hjust=0))+
   theme_minimal()+
   labs(x = "Palabra", 
        y = "Frecuencia de las palabras", 
        title = "Palabras mas repetidas")
 
+#Grafico de nube palabras
 wordcloud(rtext_corpus, 
           max.words=150, scale=c(3,.9),random.order=FALSE, rot.per=0.30, 
           colors=brewer.pal(6, "BrBG"))
@@ -58,58 +67,3 @@ wordcloud(rtext_corpus,
 
 
 #___________________________________________________________________________________________________________
-
-rtext <- readtext("D:/Mineria de datos/PROYECTO FINAL/Trend_Tecnologies_Rstudio/PDF Technology")%>%
-  tolower() %>%
-  removeWords(stopwords("english")) %>% 
-  strsplit(" ") %>%
-  unlist() %>% #producir un vector 
-  stemDocument()%>%
-  removeNumbers()%>%
-  stripWhitespace()%>%
-  removePunctuation()%>%
-  str_replace_all("[^[:alpha:][:space:]]*", "") 
-# eliminar palabras determinadas por cuenta propia.
-
-
-#Eliminar los pescios en blanco & stopwords
-myStopwords <- c(stopwords('english'),"will","use","can","technolog","new")# to add words
-rtext = rtext[!rtext %in% c("",myStopwords)]
-
-#crear elcorpus despues de la limpieza de texto
-rtext_corpus <- VCorpus(VectorSource(rtext))
-#rtext_corpus_clean <- tm_map(rtext_corpus, removeWords,myStopwords ) 
-#rtext_data_dtm <- DocumentTermMatrix(rtext_corpus_clean)
-#comentarios_palabras <- rtext_data_dtm$dimnames
-
-#Tabla para crear la frecuencia de palabras
-wordfreqs <- rtext %>%
-  table() %>%
-  as.data.frame() %>%
-  arrange(desc(Freq))
-colnames(wordfreqs) <- c("Palabras", "Frecuencia")
-head(wordfreqs)
-
-#Las 15 palabras mas frecuentes y sus frecuencias para graficar del 1 al 15
-wfd <- table(rtext)
-wfd <- wfd[order(wfd, decreasing = T)]
-wfd <- wfd[1:15]
-View(wfd)
-#Pasar a dataframe para generar visualizaciones con GGPLOT
-wfd <- as.data.frame(wfd)
-
-#Grafico en barras para ver las palabras mas comun
-ggplot(data=wfd, aes(x=rtext, y=Freq)) +
-  geom_bar(stat="identity", fill="steelblue")+
-  geom_text(aes(label=Freq), vjust=1.6, color="white", size=3.5)+
-  theme_minimal()+
-  labs(x = "Palabra", 
-       y = "Frecuencia de las palabras", 
-       title = "Palabras mas repetidas")
-
-# create wordcloud
-wordcloud(words = wordfreqs$Palabras, freq = wordfreqs$Frecuencia, 
-          max.words=150, scale=c(3,.9),random.order=FALSE, rot.per=0.30, 
-          colors=brewer.pal(6, "BrBG"))
-
-
